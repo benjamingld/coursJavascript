390f7e92d216d2c0b7dae80b10d9466002699824
Anthony PARIS <contact@pixel-online.fr>
2022-05-31 09:32:17 +0200

    ok

--- dom/exemple/js/verification.js
+++ dom/exemple/js/verification.js
@@ -1,6 +1,7 @@
 let verif = function(){
     document.getElementById("form_connexion").addEventListener("submit",function(e){
-        e.preventDefault();
+         e.preventDefault();
+        let erreur = "";
         let regex_nombre = /^[a-zA-Z]{2,}$/;
         //VERIFIER EMAIL
         console.log(this.email.value);
@@ -11,8 +12,37 @@ let verif = function(){
         } else {
             console.log("MOINS de 2 caracteres ou ce n'est pas des lettres");
         }
+        //VERIFICATION SI CHAMPS INPUT VIDE
+        if(this.email.value==""){
+            console.log('MAIL VIDE');
+        } else {
+            console.log('MAIL NON VIDE');
+        }
+        //VERIFICATION SI CASE CHECKBOX EST COCHE (QUAND UNIQUE)
+        if(this.remember.checked)
+            console.log("Case coché");
+         else 
+            console.log("case non coché");
+        
+        //VERIFICATION BOUTON RADIO
+        this.optradio.forEach(function(elm,index){
+            if(elm.checked==true){
+                console.log(elm.value);
+            }
+        });
+
+        //VERIFICATION PLUSIEURS CASES A COCHER
+        console.log(this.check);
+        this.check.forEach(function(elm,index){
+            if(elm.checked==true){
+                console.log(elm.value);
+            }
+        });
 
 
-
+        //VERIFICATION SELECT
+        console.log(this.elements['sel1'].selectedIndex, this.elements['sel1'].value)
+        //VALIDATION FORMULAIRE
+        this.submit();
     });
 }
