b41bffe1950bcc564ba06a2be79904d5f82f22aa
Anthony PARIS <contact@pixel-online.fr>
2022-06-02 09:53:11 +0200

    ok

--- tp/tp-dom/js/app.js
+++ tp/tp-dom/js/app.js
@@ -1,7 +1,7 @@
 // Cette fonction permet d'intervertir deux images en elles on lance le process 100 fois pour simuler un vrai comportement aléatoire
 function initGame(){
     console.log("--DEPART--");
-    for(let i=0;i<1000;i++){
+    for(let i=0;i<100;i++){
         //Math.random() donne une variable aleatoire entre 0 et 1 sortant *16   
         //Math ceil -> arrondi entier sup 
         const N1 = Math.ceil(16*Math.random());
